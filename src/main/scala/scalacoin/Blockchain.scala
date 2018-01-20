2600183222bef4a4ec5c4cea41d363c05902d0e9
Matteo Meli <matteo.meli@gmail.com>
2018-01-20 16:53:19 +0000

    Add initial Blockchain implementation

---
+++ src/main/scala/scalacoin/Blockchain.scala
@@ -0,0 +1,41 @@
+package scalacoin
+
+import Sha256._
+
+case class Block(index: Long, previousHash: String, timestamp: Long, data: String)
+
+object Block {
+  def hash(block: Block): String = {
+    digest(s"$block.index$block.previousHash$block.timestamp$block.data")
+  }
+
+  def isValidBlock(block: Block, previousBlock: Block) =
+    block.index == previousBlock.index + 1 && block.previousHash == hash(previousBlock)
+
+  def isValidChain(blocks: List[Block]): Boolean =
+    blocks.zip(blocks.tail).forall { case (block, previousBlock) => isValidBlock(block, previousBlock) }
+}
+
+class Blockchain(val blocks: List[Block]) {
+  import Block._
+
+  def lastBlock: Block = blocks.head
+
+  def isValid: Boolean = isValidChain(blocks)
+
+  def addBlock(block: Block): Blockchain =
+    if (isValidBlock(block, lastBlock)) Blockchain(block :: blocks) else this
+
+  def createBlock(data: String): Block = {
+    val previousBlock = lastBlock
+    Block(previousBlock.index + 1, hash(previousBlock), System.currentTimeMillis / 1000, data)
+  }
+}
+
+object Blockchain {
+  val genesisBlock = Block(0, "", System.currentTimeMillis / 1000, "Genesis Block")
+
+  def apply(): Blockchain = new Blockchain(List(genesisBlock))
+
+  def apply(blocks: List[Block]): Blockchain = new Blockchain(blocks)
+}
---
+++ src/main/scala/scalacoin/Sha256.scala
@@ -0,0 +1,10 @@
+package scalacoin
+
+import java.security.MessageDigest
+
+object Sha256 {
+  def digest(s: String): String =
+    MessageDigest.getInstance("SHA-256")
+      .digest(s.getBytes("UTF-8"))
+      .map("%02x".format(_)).mkString
+}
