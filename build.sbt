6cd8a01e2a6a34afa14d12aa73d9ec93ce56a030
Matteo Meli <matteo.meli@gmail.com>
2018-01-21 18:03:06 +0000

    Add first JSON endpoint to request the blockchain

--- build.sbt
+++ build.sbt
@@ -8,6 +8,9 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.9",
   "com.typesafe.akka" %% "akka-stream" % "2.5.9",
   "com.typesafe.akka" %% "akka-http" % "10.0.11",
+  "io.circe" %% "circe-core" % "0.9.0",
+  "io.circe" %% "circe-generic" % "0.9.0",
+  "de.heikoseeberger" %% "akka-http-circe" % "1.20.0-RC1",
   "com.typesafe.akka" %% "akka-testkit" % "2.5.9" % Test,
   "com.typesafe.akka" %% "akka-stream-testkit" % "2.5.9" % Test,
   "com.typesafe.akka" %% "akka-http-testkit" % "10.0.11" % Test
--- src/main/scala/scalacoin/blockchain/Blockchain.scala
+++ src/main/scala/scalacoin/blockchain/Blockchain.scala
@@ -1,40 +1,51 @@
 package scalacoin.blockchain
 
+import cats.syntax.either._
+
 case class Block(index: Long, previousHash: String, timestamp: Long, data: String)
+
+object GenesisBlock extends Block(0, "0", System.currentTimeMillis / 1000, "Genesis Block")
 
 object Block {
   import scalacoin.crypto._
 
   def hash(block: Block): String =
-    Sha256.digest(s"$block.index$block.previousHash$block.timestamp$block.data")
-
-  def isValidBlock(block: Block, previousBlock: Block) =
-    block.index == previousBlock.index + 1 && block.previousHash == hash(previousBlock)
-
-  def isValidChain(blocks: List[Block]): Boolean =
-    blocks.zip(blocks.tail).forall { case (block, previousBlock) => isValidBlock(block, previousBlock) }
+    Sha256.digest(s"$block.index:$block.previousHash:$block.timestamp:$block.data")
 }
 
 class Blockchain(val blocks: List[Block]) {
   import Block._
+  import Blockchain._
 
   def lastBlock: Block = blocks.head
+  def firstBlock: Block = blocks.last
 
   def isValid: Boolean = isValidChain(blocks)
 
-  def addBlock(block: Block): Blockchain =
-    if (isValidBlock(block, lastBlock)) Blockchain(block :: blocks) else this
+  def addBlock(data: String): Either[Exception, Blockchain] = addBlock(createNextBlock(data))
 
-  def createBlock(data: String): Block = {
+  def addBlock(block: Block): Either[Exception, Blockchain] =
+    if (isValidBlock(block, lastBlock)) Right(new Blockchain(block :: blocks))
+    else Left(new IllegalArgumentException("Invalid block added."))
+
+  def createNextBlock(data: String): Block = {
     val previousBlock = lastBlock
     Block(previousBlock.index + 1, hash(previousBlock), System.currentTimeMillis / 1000, data)
   }
 }
 
 object Blockchain {
-  val genesisBlock = Block(0, "", System.currentTimeMillis / 1000, "Genesis Block")
+  import Block._
 
-  def apply(): Blockchain = new Blockchain(List(genesisBlock))
+  def apply(): Blockchain = new Blockchain(List(GenesisBlock))
 
-  def apply(blocks: List[Block]): Blockchain = new Blockchain(blocks)
+  def apply(blocks: List[Block]): Either[Exception, Blockchain] =
+    if (isValidChain(blocks)) Right(new Blockchain(blocks))
+    else Left(new IllegalArgumentException("Invalid chain specified."))
+
+  def isValidBlock(block: Block, previousBlock: Block) =
+    block.index == previousBlock.index + 1 && block.previousHash == hash(previousBlock)
+
+  def isValidChain(blocks: List[Block]): Boolean =
+    blocks.zip(blocks.tail).forall { case (block, previousBlock) => isValidBlock(block, previousBlock) }
 }
